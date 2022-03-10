7aad3b9f36d3cc0b6107985b252c0294dd6445ef
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-10 16:56:52 -0500

    inserting and deleting items to db

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
+/*---------------------------------------------------------------
+-                       Express middleware
+---------------------------------------------------------------*/
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
+/*---------------------------------------------------------------
+-                       Connect to database
+---------------------------------------------------------------*/
 
 const db = mysql.createConnection(
   {
@@ -23,17 +27,65 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
+/*---------------------------------------------------------------
+-                       GET ALL candidates
+---------------------------------------------------------------*/
+
 db.query(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
 });
 
-app.get('/', (req, res) => {
+/*---------------------------------------------------------------
+-                       GET a single candidate
+---------------------------------------------------------------*/
+
+/* db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+ */
+/*---------------------------------------------------------------
+-                      Delete a candidate
+---------------------------------------------------------------*/
+
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+/*---------------------------------------------------------------
+-                      Create a candidate
+---------------------------------------------------------------*/
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+/*---------------------------------------------------------------
+-                     PASTE "Hello World on HTML"
+---------------------------------------------------------------*/
+
+/* app.get('/', (req, res) => {
   res.json({
     message: 'Hello World'
   });
-});
+}); */
 
-// Default response for any other request (Not Found)
+
+/*---------------------------------------------------------------
+-      Default response for any other request (Not Found)
+---------------------------------------------------------------*/
+
 app.use((req, res) => {
   res.status(404).end();
 });
