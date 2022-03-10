90c3e34d7088a978ffd85c76ed6d163514e3cb3f
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-10 16:47:06 -0500

    connected to the database and printint values

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'X9s0R0xpto#i7rzX8$*',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 app.get('/', (req, res) => {
   res.json({
