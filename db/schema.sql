24c0f8cdfaf43bd52fcbb42e224ffa251b1b3fbf
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-20 18:11:18 -0400

    created parties table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
+
+
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  party_name VARCHAR(50) NOT NULL,
+  party_description TEXT
+);
