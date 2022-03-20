41826c3b5ce6e171b65e810f3d8d6d24c8f09dc0
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-20 17:20:11 -0400

    add CRUD routes

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const inputCheck = require('./utils/inputCheck');
+
+
+
 /*---------------------------------------------------------------
 -                       Express middleware
 ---------------------------------------------------------------*/
@@ -12,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 /*---------------------------------------------------------------
--                       Connect to database
+-                       CONNECT to database
 ---------------------------------------------------------------*/
 
 const db = mysql.createConnection(
@@ -24,6 +28,10 @@ const db = mysql.createConnection(
   },
   console.log('Connected to the election database.')
 );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 /*---------------------------------------------------------------
 -                       GET ALL candidates
@@ -65,7 +73,7 @@ app.get('/api/candidate/:id', (req, res) => {
 });
 
 /*---------------------------------------------------------------
--                      Delete a candidate
+-                      DELETE a candidate
 ---------------------------------------------------------------*/
 
 app.delete('/api/candidate/:id', (req, res) => {
@@ -90,20 +98,30 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 /*---------------------------------------------------------------
--                      Create a candidate
+-                      CREATE a candidate
 ---------------------------------------------------------------*/
 
-/* const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
   }
-  console.log(result);
-}); */
 
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
 
 /*---------------------------------------------------------------
 -                     PASTE "Hello World on HTML"
