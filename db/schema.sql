d0406fab5949d51932ef7ee822755235c49d6c12
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-20 20:30:39 -0400

    voters table added

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS candidates;
-
-
+DROP TABLE IF EXISTS parties;
 
 
 CREATE TABLE parties (
@@ -19,4 +18,10 @@ CREATE TABLE candidates (
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
-
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
